1a2f3bce69bf28681e7bf6bda6fb69c8a433527e
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-04-20 22:17:42 +0200

    Basic dice visuals (missing D10)

--- app/src/main/java/com/example/rollthedice/dice.kt
+++ app/src/main/java/com/example/rollthedice/dice.kt
@@ -5,6 +5,9 @@ import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.print.PrintAttributes.Margins
 import android.util.Log
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
@@ -23,10 +26,12 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -45,10 +50,26 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.withFrameMillis
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.getSystemService
@@ -64,6 +85,7 @@ import dev.ricknout.composesensors.rememberSensorValueAsState
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.util.Locale
 import kotlin.math.abs
 import kotlin.math.floor
 import kotlin.math.round
@@ -74,7 +96,7 @@ import kotlin.random.Random
 fun Dice() {
     var selectedDice by rememberSaveable { mutableStateOf<String?>(null) }
     var rolling by rememberSaveable { mutableStateOf(false) }
-    var result by remember { mutableStateOf<Int?>(null) }
+    var result by rememberSaveable { mutableStateOf<Int?>(null) }
 
     fun onDiceSelected(newDice: String?) {
         selectedDice = newDice
@@ -82,25 +104,25 @@ fun Dice() {
     }
 
     fun onRollingStart() {
+        if (selectedDice == null) return
         rolling = true
         result = null
     }
 
     fun onRollingEnd() {
         rolling = false
-        if (selectedDice == null) return
-
         val sides = selectedDice!!.substring(1).toInt()
         result = Random.nextInt(1, sides + 1)
     }
 
-    Scaffold (
+    Scaffold(
         topBar = {
             TopAppBar(title = { Text("Wybierz rodziej kostki") })
         }
     ) {
         Column(Modifier.padding(it)) {
             DiceSelector(selectedDice, ::onDiceSelected)
+
             Column(
                 modifier = Modifier
                     .padding(Dp(30.0F))
@@ -114,12 +136,15 @@ fun Dice() {
                         .weight(1f)
                         .fillMaxWidth()
                 ) {
-                    if (rolling) Text("Rzucanie...")
-                    if (result != null) Text(result.toString(), fontSize = 48.sp)
+                    if (rolling) {
+                        CircularProgressIndicator(Modifier.padding(bottom = 10.dp))
+                        Text("Rzucanie...")
+                    }
+//                    if (result != null) Text(result.toString(), fontSize = 48.sp)
+                    if (result != null) DiceVisual(selectedDice, result!!)
                 }
                 DiceTrigger(rolling, ::onRollingStart, ::onRollingEnd);
             }
-
         }
     }
 }
@@ -184,5 +209,40 @@ fun DiceTrigger(rolling: Boolean, startRoll: () -> Unit, endRoll: () -> Unit) {
             Icon(imageVector = Icons.Filled.Stop, contentDescription = "Zatrzymaj")
             Text("Zatrzymaj", Modifier.padding(start = 8.dp))
         }
+    }
+}
+
+@Composable
+fun DiceVisual(selectedDice: String?, result: Int) {
+    val context = LocalContext.current
+    val drawableId = remember(selectedDice) {
+        context.resources.getIdentifier(
+            selectedDice!!.lowercase(Locale.ROOT),
+            "drawable",
+            context.packageName
+        )
+    }
+
+    var sizeImage by remember { mutableStateOf(IntSize.Zero) }
+    val gradient = Brush.radialGradient(0f to Color(0x00000000), 1f to MaterialTheme.colorScheme.surface)
+
+    Box {
+        Image(painter = painterResource(drawableId), contentDescription = "Kostka")
+        Text(
+            result.toString(),
+            color = MaterialTheme.colorScheme.primary,
+            style = TextStyle.Default.copy(
+                fontSize = 90.sp,
+                drawStyle = Stroke(
+                    miter = 10f,
+                    width = 7f,
+                    join = StrokeJoin.Round
+                )
+            ),
+            modifier = Modifier.align(Alignment.Center).onGloballyPositioned {
+                sizeImage = it.size
+            }
+        )
+//        Box(modifier = Modifier.matchParentSize().background(gradient))
     }
 }
