7689ff8b3d751f4e8b5a1f31dc038f4085c33c9c
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-04-20 18:12:14 +0200

    Character details page

--- app/src/main/java/com/example/rollthedice/characters/characterDetailsView.kt
+++ app/src/main/java/com/example/rollthedice/characters/characterDetailsView.kt
@@ -1,7 +1,13 @@
 package com.example.rollthedice.characters
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Casino
@@ -11,15 +17,24 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.rollthedice.LocalNavController
+import com.example.rollthedice.characters.ui.StatBox
+import com.example.rollthedice.characters.ui.Stats
+import com.example.rollthedice.ui.components.Dropdown
 import com.example.rollthedice.utilities.LocalSnackbarController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,7 +50,7 @@ fun CharacterDetailsView(characterName: String) {
         return;
     }
 
-    Scaffold (
+    Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text(characterName) },
@@ -48,13 +63,13 @@ fun CharacterDetailsView(characterName: String) {
                     }
                 },
                 actions = {
-                    IconButton(onClick = {  }) {
+                    IconButton(onClick = { }) {
                         Icon(
                             imageVector = Icons.Filled.Edit,
                             contentDescription = "Edytuj",
                         )
                     }
-                    IconButton(onClick = {  }) {
+                    IconButton(onClick = { }) {
                         Icon(
                             imageVector = Icons.Filled.Delete,
                             contentDescription = "Usuń",
@@ -69,8 +84,46 @@ fun CharacterDetailsView(characterName: String) {
             }
         }
     ) {
-        Column (Modifier.padding(it)) {
-            Text(character!!.race.toString())
+        Row(
+            modifier = Modifier
+                .padding(it)
+                .padding(horizontal = 10.dp)
+                .fillMaxWidth()
+        ) {
+            Stats(character!!.stats)
+
+            Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                Row (horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                    Text(
+                        character!!.race.toString(),
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(5.dp))
+                            .background(MaterialTheme.colorScheme.surfaceVariant)
+                            .padding(8.dp)
+                    )
+                    Text(
+                        character!!.characterClass.toString(),
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(5.dp))
+                            .background(MaterialTheme.colorScheme.surfaceVariant)
+                            .padding(8.dp)
+                    )
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp)
+                ) {
+                    StatBox(label = "Zbroja", value = "1")
+                    StatBox(label = "Incjatywa", value = "4")
+                    StatBox(label = "Szybkość", value = "5")
+                }
+            }
+
         }
     }
 }
